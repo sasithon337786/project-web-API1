7b3c1be105dae8db33409c22b551078ac9f1e6c6
CAT <bigstirnabon@gmail.com>
2024-03-02 00:23:01 +0700

    t

--- api/pictrue.ts
+++ api/pictrue.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { conn, queryAsync } from "../dbconnect";
-
+import mysql from "mysql";
+import { PictruepostResponse } from "../mode/PictruepostResponse";
 
 
 export const router = express.Router();
@@ -13,3 +14,31 @@ router.get("/all", (req,res)=>{
         })
     }
 );
+
+router.put("/:id", async (req, res) => {
+    let id = +req.params.id;
+    let pictrue: PictruepostResponse = req.body;
+    let pictrueOriginal: PictruepostResponse | undefined;
+  
+    let sql = mysql.format("select * from pictrue where pictrue_id = ?", [id]);
+  
+    let result = await queryAsync(sql);
+    const rawData = JSON.parse(JSON.stringify(result));
+    console.log(rawData);
+    pictrueOriginal = rawData[0] as PictruepostResponse;
+    console.log(pictrueOriginal);
+  
+    let updatepictrue = {...pictrueOriginal, ...pictrue};
+    console.log(pictrue);
+    console.log(updatepictrue);
+      sql =
+        "update  `pictrue` set `pictrue_p`=? where `pictrue_id`=?";
+      sql = mysql.format(sql, [
+        pictrue.pictrue_p,
+        id,
+      ]);
+      conn.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(201).json({ affected_row: result.affectedRows });
+      });
+  });
---
+++ mode/PictruepostResponse.ts
@@ -0,0 +1,6 @@
+export interface PictruepostResponse {
+    pictrue_id:  number;
+    pictrue_url: string;
+    pictrue_p:   number;
+    u_id:        number;
+}
