0c610ba2340d970e400fbb857a42b145c61417a0
CAT <bigstirnabon@gmail.com>
2024-03-01 20:53:57 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import {router as user} from "./api/user";
 export const app = express();
-
+console.log("asdas");
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 
