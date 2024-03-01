6dbb3b3bc80977351ab9eee8cfcffa87eadd02b2
CAT <bigstirnabon@gmail.com>
2024-03-01 21:46:55 +0700

    test

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import {router as user} from "./api/user";
+import cors from "cors";
 export const app = express();
 console.log("asdas");
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 
+
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
 app.use("/", user)
